3ac5938b7c05ed27ba9668069fc7ca04fd7fac7a
Asbah Ishaq <asbah.ishaq@7vals.com>
2017-06-13 10:34:29 +0500

    2nd commit

---
+++ app/views/answers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @answer, :id, :detail, :created_at, :updated_at
---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170612073561) do
+
+  create_table "answers", force: true do |t|
+    t.text     "detail"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "question_id"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
+
+  create_table "comments", force: true do |t|
+    t.string   "detail"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.integer  "answer_id"
+  end
+
+  add_index "comments", ["answer_id"], name: "index_comments_on_answer_id"
+  add_index "comments", ["question_id"], name: "index_comments_on_question_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "questions", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
